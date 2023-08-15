1e2844a552235beec96c46a59243302c3f1f2756
Musharraf188 <mr.musharraf188@gmail.com>
2023-08-15 10:53:18 +0530

    side-menu

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// <<<<<<< HEAD
+// import Login from "./components/Login/Login";
+// import MainHeader from "./components/MainHeader/MainHeader";
+
+// const App =()=>{
+//   return <div>
+//     <MainHeader />
+//     <Login />
+//   </div>
+// =======
+
+import LeftMenuBar from "./components/leftMenu/LeftMenuBar";
+
+function App() {
+  return (
+    <div className="App">
+      <LeftMenuBar />
+    </div>
+  );
+  }
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+// <<<<<<< HEAD
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+
+// =======
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+// >>>>>>> db02744122ce4d9ea273bf70ddc0183ebc40157d
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+// <<<<<<< HEAD
+// =======
+
+// reportWebVitals();
+// >>>>>>> db02744122ce4d9ea273bf70ddc0183ebc40157d
